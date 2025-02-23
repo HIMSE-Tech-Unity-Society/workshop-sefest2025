bcc37cd308d233b9d0cc32020e3503bae915aeff
rsiyun <rsiyun@gmail.com>
2025-02-23 08:01:32 +0700

    add user and logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,110 +7,180 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
- /**
-     * @OA\Post (
-     *     path="/api/login",
-     *     tags={"Auth"},
-     *     @OA\RequestBody(
-     *         @OA\MediaType(
-     *             mediaType="application/json",
-     *             @OA\Schema(
-     *                 @OA\Property(
-     *                      type="object",
-     *                      @OA\Property(
-     *                          property="email",
-     *                          type="string"
-     *                      ),
-     *                      @OA\Property(
-     *                          property="password",
-     *                          type="string"
-     *                      )
-     *                 ),
-     *                 example={
-     *                     "email":"email@sefest.com",
-     *                     "password":"PasswordSefest2025"
-     *                }
-     *             )
-     *         )
-     *      ),
-     *      @OA\Response(
-     *          response=200,
-     *          description="Success",
-     *          @OA\JsonContent(
-     *              @OA\Property(property="message", type="string", example="Login Success"),
-     *              @OA\Property(property="access_token", type="string", example="1|widjwijdiwjdialdjaiwjdiwjd"),
-     *              @OA\Property(property="token_type", type="string", example="Bearer"),
-     *          )
-     *      ),
-     * ),
-     * @OA\Post (
-     *     path="/api/register",
-     *     tags={"Auth"},
-     *     @OA\RequestBody(
-     *         @OA\MediaType(
-     *             mediaType="application/json",
-     *             @OA\Schema(
-     *                 @OA\Property(
-     *                      type="object",
-     *                      @OA\Property(
-     *                          property="name",
-     *                          type="string"
-     *                      ),
-     *                      @OA\Property(
-     *                          property="email",
-     *                          type="string"
-     *                      ),
-     *                      @OA\Property(
-     *                          property="password",
-     *                          type="string"
-     *                      ),
-     *                      @OA\Property(
-     *                          property="job",
-     *                          type="string"
-     *                      ),
-     *                      @OA\Property(
-     *                          property="bank_name",
-     *                          type="string"
-     *                      ),
-     *                      @OA\Property(
-     *                          property="account_number",
-     *                          type="string"
-     *                      ),
-     *                      @OA\Property(
-     *                          property="account_owner_name",
-     *                          type="string"
-     *                      ),
-     *                      @OA\Property(
-     *                          property="avatar",
-     *                          type="binary"
-     *                      )
-     *                 ),
-     *                 example={
-     *                     "name":"John",
-     *                     "email":"john@sefest.com",
-     *                     "password":"PasswordSefest2025",
-     *                     "job":"a student and freelancer full-stack engineer from indonesia, specialized in web development",
-     *                     "bank_name":"bca",
-     *                     "account_number":"1201220011",
-     *                     "account_owner_name":"johndoe",
-     *                     "avatar":"image.png",
-     *                }
-     *             )
-     *         )
-     *      ),
-     *      @OA\Response(
-     *          response=201,
-     *          description="Success",
-     *          @OA\JsonContent(
-     *                  @OA\Property(property="message", type="string", example="akun berhasil dibuat, silahkan ke halman login"),
-     *              ),
-     *          )
-     *      ),
-     * )
-    */
+/**
+ * @OA\Post (
+ *     path="/api/login",
+ *     tags={"Auth"},
+ *     @OA\RequestBody(
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(
+ *                 @OA\Property(
+ *                      type="object",
+ *                      @OA\Property(
+ *                          property="email",
+ *                          type="string"
+ *                      ),
+ *                      @OA\Property(
+ *                          property="password",
+ *                          type="string"
+ *                      )
+ *                 ),
+ *                 example={
+ *                     "email":"email@sefest.com",
+ *                     "password":"PasswordSefest2025"
+ *                }
+ *             )
+ *         )
+ *      ),
+ *      @OA\Response(
+ *          response=200,
+ *          description="Success",
+ *          @OA\JsonContent(
+ *              @OA\Property(property="message", type="string", example="Login Success"),
+ *              @OA\Property(property="access_token", type="string", example="1|widjwijdiwjdialdjaiwjdiwjd"),
+ *              @OA\Property(property="token_type", type="string", example="Bearer"),
+ *          )
+ *      ),
+ * ),
+ * @OA\Post (
+ *     path="/api/register",
+ *     tags={"Auth"},
+ *     @OA\RequestBody(
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(
+ *                 @OA\Property(
+ *                      type="object",
+ *                      @OA\Property(
+ *                          property="name",
+ *                          type="string"
+ *                      ),
+ *                      @OA\Property(
+ *                          property="email",
+ *                          type="string"
+ *                      ),
+ *                      @OA\Property(
+ *                          property="password",
+ *                          type="string"
+ *                      ),
+ *                      @OA\Property(
+ *                          property="job",
+ *                          type="string"
+ *                      ),
+ *                      @OA\Property(
+ *                          property="bank_name",
+ *                          type="string"
+ *                      ),
+ *                      @OA\Property(
+ *                          property="account_number",
+ *                          type="string"
+ *                      ),
+ *                      @OA\Property(
+ *                          property="account_owner_name",
+ *                          type="string"
+ *                      ),
+ *                      @OA\Property(
+ *                          property="avatar",
+ *                          type="binary"
+ *                      )
+ *                 ),
+ *                 example={
+ *                     "name":"John",
+ *                     "email":"john@sefest.com",
+ *                     "password":"PasswordSefest2025",
+ *                     "job":"a student and freelancer full-stack engineer from indonesia, specialized in web development",
+ *                     "bank_name":"bca",
+ *                     "account_number":"1201220011",
+ *                     "account_owner_name":"johndoe",
+ *                     "avatar":"image.png",
+ *                }
+ *             )
+ *         )
+ *      ),
+ *      @OA\Response(
+ *          response=201,
+ *          description="Success",
+ *          @OA\JsonContent(
+ *                  @OA\Property(property="message", type="string", example="akun berhasil dibuat, silahkan ke halman login"),
+ *              ),
+ *          )
+ *      ),
+ * )
+ * @OA\Post (
+ *     path="/api/logout",
+ *     tags={"Auth"},
+ *      @OA\Response(
+ *          response=200,
+ *          description="Success",
+ *          @OA\JsonContent(
+ *                  @OA\Property(property="message", type="string", example="logout sukses, terimakasih!"),
+ *              ),
+ *          )
+ *      ),
+ * )
+ * @OA\Get (
+ *     path="/api/user",
+ *     tags={"Auth"},
+ *      @OA\Response(
+ *          response=200,
+ *          description="Success",
+ *          @OA\JsonContent(
+ *                  @OA\Property(
+ *                      type="object",
+ *                      @OA\Property(
+ *                          property="name",
+ *                          type="string"
+ *                      ),
+ *                      @OA\Property(
+ *                          property="email",
+ *                          type="string"
+ *                      ),
+ *                      @OA\Property(
+ *                          property="password",
+ *                          type="string"
+ *                      ),
+ *                      @OA\Property(
+ *                          property="job",
+ *                          type="string"
+ *                      ),
+ *                      @OA\Property(
+ *                          property="bank_name",
+ *                          type="string"
+ *                      ),
+ *                      @OA\Property(
+ *                          property="account_number",
+ *                          type="string"
+ *                      ),
+ *                      @OA\Property(
+ *                          property="account_owner_name",
+ *                          type="string"
+ *                      ),
+ *                      @OA\Property(
+ *                          property="avatar",
+ *                          type="binary"
+ *                      )
+ *                 ),
+ *                 example={
+ *                     "name":"John",
+ *                     "email":"john@sefest.com",
+ *                     "password":"PasswordSefest2025",
+ *                     "job":"a student and freelancer full-stack engineer from indonesia, specialized in web development",
+ *                     "bank_name":"bca",
+ *                     "account_number":"1201220011",
+ *                     "account_owner_name":"johndoe",
+ *                     "avatar":"image.png",
+ *                }
+ *              ),
+ *          )
+ *      ),
+ * )
+ */
+
 class AuthController extends Controller
 {
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         $request->validate([
             "email" => "required",
             "password" => "required",
@@ -125,13 +195,13 @@ class AuthController extends Controller
         return response()->json([
             'message' => "Login Success",
             'access_token' => $token,
-            "name" => $user->name,
+            "user" => $user,
             'token_type' => 'Bearer'
         ], 200);
-
     }
 
-    public function register(Request $request){
+    public function register(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|max:255|unique:users',
@@ -143,7 +213,7 @@ class AuthController extends Controller
             'avatar' => 'nullable|image'
         ]);
         if ($validator->fails()) {
-            return response()->json(["errors" => $validator->errors()]);
+            return response()->json(["errors" => $validator->errors()], 409);
         }
         $query = [
             "name" => $request->name,
@@ -163,7 +233,19 @@ class AuthController extends Controller
         return response()->json([
             "message" => "akun berhasil dibuat, silahkan ke halman login"
         ], 201);
-
     }
 
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'logout sukses, terimakasih!',
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,27 +1,28 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::get('/category', [CategoryController::class, 'index']);
-Route::prefix('product')->group( function () {
+
+Route::prefix('product')->group(function () {
     Route::get('/all', [ProductController::class, 'index']);
     Route::get('/search', [ProductController::class, 'search']);
     Route::get('/detail/{slug}', [ProductController::class, 'show']);
 });
-Route::middleware('auth:sanctum')->group(function(){
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
     Route::post('/order', [OrderController::class, 'store']);
-    Route::prefix('creator')->group(function(){
+
+    Route::prefix('creator')->group(function () {
         Route::get('product', [ProductController::class, 'getAllbycreator']);
         Route::get('product/{slug}', [ProductController::class, 'getOnebycreator']);
         Route::post('product', [ProductController::class, 'store']);
@@ -33,5 +34,3 @@ Route::middleware('auth:sanctum')->group(function(){
         Route::put('order/approve/{order}', [OrderController::class, 'updatePaid']);
     });
 });
-
-
